06e7b58a170f5b8b50ddc8051418017ac23b92b6
Tayyab Rasheed <mr.tayyabrasheed@yahoo.com>
2025-09-03 21:31:19 +0500

    [UPDATE] updated layouts

--- website_ext/static/src/js/contact_map.js
+++ website_ext/static/src/js/contact_map.js
@@ -10,48 +10,6 @@ function loadGoogleMapsApi(callback) {
     script.onload = callback;
     document.head.appendChild(script);
 }
-
-// function initMap() {
-//     var map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 3,
-//         center: { lat: 20, lng: 0 } // world view
-//     });
-//
-//     var geocoder = new google.maps.Geocoder();
-//     var locations = [
-//         { address: "14015 Hymill Dr, Pflugerville, TX 78660, USA", title: "USA Office - Pflugerville" },
-//         { address: "Office UG-8, 9 Grand Xcito, D - Markaz, Gulberg Greens, Islamabad, Pakistan", title: "Pakistan Office - Islamabad" }
-//     ];
-//
-//     locations.forEach(function(loc) {
-//         geocoder.geocode({ address: loc.address }, function(results, status) {
-//             if (status === "OK") {
-//                 var marker = new google.maps.Marker({
-//                     position: results[0].geometry.location,
-//                     map: map,
-//                     title: loc.title
-//                 });
-//
-//                 var infoWindow = new google.maps.InfoWindow({
-//                     content: "<b>" + loc.title + "</b><br>" + loc.address
-//                 });
-//
-//                 marker.addListener("click", function() {
-//                     infoWindow.open(map, marker);
-//                 });
-//
-//                 // Auto-fit map bounds to include all markers
-//                 if (!map.bounds) {
-//                     map.bounds = new google.maps.LatLngBounds();
-//                 }
-//                 map.bounds.extend(results[0].geometry.location);
-//                 map.fitBounds(map.bounds);
-//             } else {
-//                 console.error("Geocode failed for " + loc.address + ": " + status);
-//             }
-//         });
-//     });
-// }
 function initMap() {
     var mapIds = ["map-desktop", "map-mobile"];
 
