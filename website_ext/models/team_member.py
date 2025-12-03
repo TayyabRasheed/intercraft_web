3dc172e282814e6d9d8e1ce8f9fc413778ce9df5
Tayyab Rasheed <mr.tayyabrasheed@yahoo.com>
2025-12-03 12:01:43 +0500

    [UPDATE] updated teams section

--- website_ext/models/team_member.py
+++ website_ext/models/team_member.py
@@ -4,11 +4,13 @@ from odoo import models, fields, api
 class TeamMember(models.Model):
     _name = 'website.team.member'
     _description = 'Team Member'
+    _inherit = ['image.mixin']
     _order = 'sequence, id'
 
     name = fields.Char('Name', required=True)
     role = fields.Char('Role', required=True)
-    image = fields.Image('Photo')
+    image = fields.Image('Photo', max_width=1024, max_height=1024)
+    # image = fields.Image('Photo')
     linkedin_url = fields.Char('LinkedIn URL')
     tag_ids = fields.Many2many('website.team.tag', string='Tags')
     sequence = fields.Integer('Sequence', default=10)
